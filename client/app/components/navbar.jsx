eb468a31193a126996fca1693f9f7f9b0c92e415
Manujaya <manujaya.xyz@gmail.com>
2024-03-30 11:38:12 +0530

    changes to navbar

--- client/app/components/navbar.jsx
+++ client/app/components/navbar.jsx
@@ -1,5 +1,21 @@
 'use client'
+
+import Link from "next/link"
+import { usePathname } from 'next/navigation'
+
 export default function Navbar(){
+
+    const pathName = usePathname()
+
+    const courseIcon = "";
+
+    const menus = [
+        {icon:courseIcon, name: "Courses", route:"/courses" },
+        {icon:courseIcon, name: "Students",route:"/students" },
+        {icon:courseIcon, name: "Add Students",route:"/add" },
+        {icon:courseIcon, name: "History",route:"/history" },
+        {icon:courseIcon, name: "Change Admins",route:"/access" },
+    ]
     return (
         <>
             <button data-drawer-target="default-sidebar" data-drawer-toggle="default-sidebar" aria-controls="default-sidebar" type="button" className="inline-flex items-center p-2 mt-2 ms-3 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
@@ -10,32 +26,23 @@ export default function Navbar(){
             </button>
 
             <aside id="default-sidebar" className="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
-                <div className="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
+                <div className="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-blue-900">
                     <ul className="space-y-2 font-medium">
-                        {/* Sidebar content */}
+                     
+                     {menus.map((menu, i)=>(
+                        <li>
+                        <Link key={i} href={menu.route} className={`flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-blue-500 group ${pathName.includes(menu.route) && " bg-blue-500"}`}>
+                            <svg className="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 21">
+                               {menu.icon}
+                            </svg>
+                            <span className="ms-3">{menu.name}</span>
+                        </Link>
+                         </li>
+                     ))}     
                     </ul>
                 </div>
             </aside>
 
-            <div className="p-4 sm:ml-64">
-                <div className="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700">
-                    <div className="grid grid-cols-3 gap-4 mb-4">
-                        {/* Grid content */}
-                    </div>
-                    <div className="flex items-center justify-center h-48 mb-4 rounded bg-gray-50 dark:bg-gray-800">
-                        {/* Flex content */}
-                    </div>
-                    <div className="grid grid-cols-2 gap-4 mb-4">
-                        {/* More grid content */}
-                    </div>
-                    <div className="flex items-center justify-center h-48 mb-4 rounded bg-gray-50 dark:bg-gray-800">
-                        {/* Another flex content */}
-                    </div>
-                    <div className="grid grid-cols-2 gap-4">
-                        {/* More grid content */}
-                    </div>
-                </div>
-            </div>
         </>
     );
 }
